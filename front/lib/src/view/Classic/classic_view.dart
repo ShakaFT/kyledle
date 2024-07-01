c6551b71c79fe73ec3d69d4e7ffc434cddd848fc
Florent TORNICELLI <florent@geode.com>
2024-07-01 23:55:51 +0200

    feat: attempts design

--- front/lib/src/view/Classic/classic_view.dart
+++ front/lib/src/view/Classic/classic_view.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:kyledle/src/controller/Classic/classic_controller.dart';
 import 'package:state_extended/state_extended.dart';
@@ -47,8 +48,13 @@ class _ClassicViewState extends StateX<ClassicView> {
   }
 
   @override
-  Widget build(BuildContext context) => Center(
+  Widget build(BuildContext context) {
+    const maxHeight = 100; // Define a fixed height for each item
+
+    return Center(
+      child: SingleChildScrollView(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               padding: const EdgeInsets.all(16.0),
@@ -96,53 +102,59 @@ class _ClassicViewState extends StateX<ClassicView> {
               ),
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: _textEditingController,
-                          focusNode: _focusNode,
-                          decoration: const InputDecoration(
-                            hintText: "Tape le nom du monstre",
-                            border: InputBorder.none,
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.25,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: _textEditingController,
+                            focusNode: _focusNode,
+                            decoration: const InputDecoration(
+                              hintText: "Tape le nom du monstre",
+                              border: InputBorder.none,
+                            ),
                           ),
                         ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.send, color: Colors.blueGrey),
-                        onPressed: () {
-                          // Logique pour l'envoi
-                        },
-                      ),
-                    ],
+                        IconButton(
+                          icon: const Icon(Icons.send, color: Colors.blueGrey),
+                          onPressed: () {
+                            // Logique pour l'envoi
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                   if (_showFilteredNames)
-                    Container(
-                      constraints: const BoxConstraints(maxHeight: 200),
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: _filteredNames.length,
-                        itemBuilder: (context, index) => MouseRegion(
-                          onEnter: (_) {
-                            setState(() {
-                              _hoveredItem = _filteredNames[index];
-                            });
-                          },
-                          onExit: (_) {
-                            setState(() {
-                              _hoveredItem = '';
-                            });
-                          },
-                          child: Container(
-                            color: _hoveredItem == _filteredNames[index]
-                                ? Colors.grey[300]
-                                : Colors.transparent,
-                            child: ListTile(
-                              onTap: () {
-                                _controller.addAttempt(_filteredNames[index]);
-                                _focusNode.unfocus();
-                              },
-                              title: Text(_filteredNames[index]),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      child: Container(
+                        constraints: const BoxConstraints(maxHeight: 200),
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: _filteredNames.length,
+                          itemBuilder: (context, index) => MouseRegion(
+                            onEnter: (_) {
+                              setState(() {
+                                _hoveredItem = _filteredNames[index];
+                              });
+                            },
+                            onExit: (_) {
+                              setState(() {
+                                _hoveredItem = '';
+                              });
+                            },
+                            child: Container(
+                              color: _hoveredItem == _filteredNames[index]
+                                  ? Colors.grey[300]
+                                  : Colors.transparent,
+                              child: ListTile(
+                                onTap: () {
+                                  _controller.addAttempt(_filteredNames[index]);
+                                  _focusNode.unfocus();
+                                },
+                                title: Text(_filteredNames[index]),
+                              ),
                             ),
                           ),
                         ),
@@ -151,22 +163,101 @@ class _ClassicViewState extends StateX<ClassicView> {
                 ],
               ),
             ),
-            if (_controller.attempts.isNotEmpty)
-              Column(
-                children: _controller.attempts
-                    .map(
-                      (item) => Row(
-                        children: [Card(child: Text(item))],
-                      ),
-                    )
-                    .toList(),
+            const SizedBox(height: 20),
+            if (_controller.attempts.isNotEmpty) ...[
+              // Display headers once
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:
+                    _controller.data[_controller.attempts.first]["columns"].keys
+                        .map<Widget>(
+                          (key) => Container(
+                            padding: const EdgeInsets.all(8.0),
+                            margin: const EdgeInsets.all(4.0),
+                            width: 150, // fixed width for each header
+                            decoration: BoxDecoration(
+                              color: Colors.yellow[700],
+                              borderRadius: BorderRadius.circular(8.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Text(
+                              key,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        )
+                        .toList(),
               ),
-            const SizedBox(
-              height: 10,
-            ),
+              const SizedBox(height: 10),
+              // Display attempts
+              Column(
+                children: _controller.attempts.map((attempt) {
+                  final columns = _controller.data[attempt]["columns"];
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: columns.keys.map<Widget>((key) {
+                      final value = columns[key];
+                      String displayValue;
+                      if (value is List) {
+                        displayValue = value.join(", ");
+                      } else {
+                        displayValue = value.toString().trim();
+                      }
+                      return Container(
+                        padding: const EdgeInsets.all(8.0),
+                        margin: const EdgeInsets.all(4.0),
+                        width: 150, // fixed width for each cell
+                        height:
+                            maxHeight.toDouble(), // fixed height for each cell
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: const Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: displayValue.startsWith('http')
+                              ? CachedNetworkImage(
+                                  imageUrl:
+                                      "https://static.wikia.nocookie.net/mogapedia/images/6/6d/MHWI-Alatreon_Icon.png/revision/latest?cb=20200321134438&path-prefix=fr",
+                                  placeholder: (context, url) =>
+                                      const CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                  width: 30,
+                                  height: 30,
+                                )
+                              : Text(displayValue, textAlign: TextAlign.center),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }).toList(),
+              ),
+            ],
+            const SizedBox(height: 10),
           ],
         ),
-      );
+      ),
+    );
+  }
 
   @override
   void dispose() {
