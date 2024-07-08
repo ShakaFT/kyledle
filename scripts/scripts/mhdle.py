7cd16f9e55424f892a6221326db28aae8a504743
Florent TORNICELLI <florent@geode.com>
2024-07-08 23:09:06 +0200

    tuning: Win container

--- scripts/scripts/mhdle.py
+++ scripts/scripts/mhdle.py
@@ -25,54 +25,63 @@ storage_client = storage.Client()
 bucket = storage_client.bucket(BUCKET_NAME)
 public_bucket = storage_client.bucket(PUBLIC_BUCKET_NAME)
 
-df = pd.read_csv(f"{DATA_URL}&gid={MHDLE_ID}")
 
-mhdle_classic = {
-    "characters": defaultdict(dict),
-    "columns": [
-        "Photo",
-        "Type de monstre",
-        "Éléments",
-        "Faiblesses",
-        "Afflictions",
-        "Génération",
-        "Sous-espèce",
-    ],
-    "indices": ["Taille Moyenne", "Couleur"],
-}
-for _, row in df.iterrows():
-    monster = row["Nom"].strip()
-    console.log(f"[bold green]{monster} [blue]processing...")
-    blob = public_bucket.blob(f"mhdle/{monster}.png")
+def string_to_list(value: str) -> list[str]:
+    """
+    This function casts string to list.
+    """
+    return [element.strip() for element in value.split("/")]
 
-    if "--images" in sys.argv:
-        response = requests.get(row["Photo"], timeout=60)
-        blob.upload_from_string(response.content)
 
-    mhdle_classic["characters"][monster] = {
-        "columns": {
-            "Photo": blob.public_url,
-            "Type de monstre": row["Type de monstre"].strip(),
-            "Éléments": [
-                element.strip()
-                for element in row["Éléments"].split("/")
-                if element.strip().lower() != "x"
-            ],
-            "Faiblesses": [
-                element.strip()
-                for element in row["Faiblesses"].split("/")
-                if element.strip().lower() != "x"
-            ],
-            "Génération": row["Génération"].strip(),
-            "Afflictions": [
-                element.strip()
-                for element in row["Afflictions"].split("/")
-                if element.strip().lower() != "x"
-            ],
-            "Sous-espèce": row["Sous-espèce"].strip(),
-        },
-        "indices": [str(row["Taille Moyenne"])],
+def main():
+    """
+    main function
+    """
+    df = pd.read_csv(f"{DATA_URL}&gid={MHDLE_ID}")
+
+    mhdle_classic = {
+        "characters": defaultdict(dict),
+        "columns": [
+            "Photo",
+            "Type de monstre",
+            "Éléments",
+            "Faiblesses",
+            "Afflictions",
+            "Génération",
+            "Sous-espèce",
+        ],
+        "indices": ["Taille Moyenne", "Couleurs"],
     }
+    for _, row in df.iterrows():
+        monster = row["Nom"].strip()
+        console.log(f"[bold green]{monster} [blue]processing...")
+        blob = public_bucket.blob(f"mhdle/{monster}.png")
 
-blob = bucket.blob("mhdle/classic.json")
-blob.upload_from_string(json.dumps(mhdle_classic))
+        if "--images" in sys.argv:
+            response = requests.get(row["Photo"], timeout=60)
+            response.raise_for_status()
+            blob.upload_from_string(response.content)
+
+        mhdle_classic["characters"][monster] = {
+            "columns": {
+                "Photo": blob.public_url,
+                "Type de monstre": row["Type de monstre"].strip(),
+                "Éléments": string_to_list(row["Éléments"]),
+                "Faiblesses": string_to_list(row["Faiblesses"]),
+                "Génération": row["Génération"].strip(),
+                "Afflictions": string_to_list(row["Afflictions"]),
+                "Sous-espèce": row["Sous-espèce"].strip(),
+            },
+            "indices": [
+                str(row["Taille Moyenne"]).strip(),
+                string_to_list(row["Afflictions"]),
+            ],
+        }
+
+    blob = bucket.blob("mhdle/classic.json")
+    if "--write" in sys.argv:
+        blob.upload_from_string(json.dumps(mhdle_classic))
+
+
+if __name__ == "__main__":
+    main()
