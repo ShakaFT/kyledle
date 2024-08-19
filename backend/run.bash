384940a6aa5385312420adec27c7a509fe15cade
Florent Tornicelli <florent.tornicelli@gmail.com>
2024-08-19 11:40:56 +0200

    feat: Autorestart during local development

--- backend/run.bash
+++ backend/run.bash
@@ -7,15 +7,15 @@ if [ "$1" = "prod" ]; then
     export ENVIRONMENT="prod"
     export REDIS_PORT=6381
     export WEB_PORT=8082
-    docker-compose -f docker-compose.yml -f docker-compose.prod.yml --env-file .env/.env.prod -p kyledle-prod up
+    docker-compose -f docker-compose.yml -f docker-compose.prod.yml --env-file .env/.env.prod -p kyledle-prod up --build
 elif [ "$1" = "dev" ]; then
     export ENVIRONMENT="dev"
     export REDIS_PORT=6380
     export WEB_PORT=8081
-    docker-compose -f docker-compose.yml -f docker-compose.prod.yml --env-file .env/.env.dev -p kyledle-dev up
+    docker-compose -f docker-compose.yml -f docker-compose.prod.yml --env-file .env/.env.dev -p kyledle-dev up --build
 else
     export ENVIRONMENT="local"
     export REDIS_PORT=6379
     export WEB_PORT=8080
-    docker-compose -f docker-compose.yml -p kyledle-local up
+    docker-compose -f docker-compose.yml -p kyledle-local up --build
 fi
