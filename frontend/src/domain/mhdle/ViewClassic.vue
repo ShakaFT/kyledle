8480180209aa2f27fc6abcf5cfeb009669e0e300
William CHAZOT <william@macbook>
2024-09-27 00:11:12 +0200

    wip: first display of victory dialog

--- frontend/src/domain/mhdle/ViewClassic.vue
+++ frontend/src/domain/mhdle/ViewClassic.vue
@@ -1,30 +1,44 @@
 <script setup lang="ts">
   import type { MHdleCharacter } from '@/types/mhdle.types';
 
+  import { watchDebounced } from '@vueuse/core';
   import { computed, ref } from 'vue';
 
   import BaseLayout from '@/core/layouts/BaseLayout.vue';
   import AttemptsTable from '@/domain/mhdle/components/AttemptsTable.vue';
   import SearchSelect from '@/domain/mhdle/components/SearchSelect.vue';
+  import VictoryDialog from '@/domain/mhdle/components/VictoryDialog.vue';
   import { useCharacters } from '@/stores/GameStore';
   import { useTarget } from '@/stores/ModeStore';
 
   const { characters } = useCharacters<MHdleCharacter>();
-  useTarget<MHdleCharacter>();
+  const { target } = useTarget<MHdleCharacter>();
 
   const leftovers = computed(() => characters.value.filter(isNotAttempted));
   const attempts = ref<MHdleCharacter[]>([]);
+  const isTargetFound = ref(false);
 
   const isNotAttempted = (character: MHdleCharacter): boolean =>
     !attempts.value.map((attempt) => attempt.id).includes(character.id);
 
   const attemptOf = (character: MHdleCharacter) =>
     attempts.value.unshift(character);
+
+  watchDebounced(
+    attempts.value,
+    () => {
+      isTargetFound.value = !!attempts.value.find(
+        (attempt) => attempt.id === target.value.id,
+      );
+    },
+    { debounce: 3500 },
+  );
 </script>
 
 <template>
   <BaseLayout>
     <SearchSelect :characters="leftovers" @select="attemptOf" />
-    <AttemptsTable :attempts="attempts" />
+    <AttemptsTable :attempts />
+    <VictoryDialog :count="attempts.length" :open="isTargetFound" />
   </BaseLayout>
 </template>
---
+++ frontend/src/domain/mhdle/components/VictoryDialog.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  defineProps<{ count: number; open: boolean }>();
+</script>
+
+<template>
+  <dialog :open>
+    <div
+      class="fixed inset-0 flex h-screen -rotate-3 flex-col items-center justify-center"
+    >
+      <div
+        :class="[
+          'animate-duration-300',
+          'animate-jump-in',
+          'bg-slate-800',
+          'drop-shadow-lg',
+          'p-9',
+          'rounded-lg',
+          'text-center',
+        ]"
+      >
+        <h1 class="font-[BluuNext] text-5xl text-green-400">Bravo !</h1>
+        <p class="font-[BluuNext] text-xl text-green-400">
+          Vous avez trouvé le monstre du jour en {{ count }} tentative(s).
+        </p>
+      </div>
+    </div>
+  </dialog>
+</template>
