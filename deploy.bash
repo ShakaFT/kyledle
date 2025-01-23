92fe802062110695d9a2d5bee71302a5c6669b0e
Florent Tornicelli <florent.tornicelli@gmail.com>
2025-01-23 17:31:55 +0100

    chore: wip

--- deploy.bash
+++ deploy.bash
@@ -6,9 +6,11 @@ git pull
 if [ "$1" = "prod" ]; then
     sudo supervisorctl restart kyledle-prod-backend
     sudo supervisorctl restart kyledle-prod-frontend
+    sudo supervisorctl restart kyledle-prod-nginx
 elif [ "$1" = "dev" ]; then
     sudo supervisorctl restart kyledle-dev-backend
     sudo supervisorctl restart kyledle-dev-frontend
+    sudo supervisorctl restart kyledle-dev-nginx
 else
     echo "Exit! Missing first parameter"
     exit 1
