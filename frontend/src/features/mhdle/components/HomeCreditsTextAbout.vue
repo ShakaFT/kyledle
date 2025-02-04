2be49a5d55a761b088ce67c141d13383106309a3
William CHAZOT <william@macbook>
2025-02-04 22:09:21 +0100

    fix: hide unreleased game modes

--- frontend/src/features/mhdle/components/HomeCreditsTextAbout.vue
+++ frontend/src/features/mhdle/components/HomeCreditsTextAbout.vue
@@ -6,7 +6,17 @@
 <template>
   <AppTitle class="mb-6" keypath="mhdle.credits.about" />
 
-  <AppParagraph keypath="mhdle.credits.about.description1" />
+  <AppParagraph
+    :args="[
+      {
+        slot: 'link',
+        tag: 'a',
+        href: 'https://pokedle.net/',
+        term: 'Pokédle',
+      },
+    ]"
+    keypath="mhdle.credits.about.description1"
+  />
   <AppParagraph keypath="mhdle.credits.about.description2" />
   <AppParagraph
     :args="[
